cb74d849311009d3b02ed0566bb02821aee7726b
Emily Landers <emily.j.landers@gmail.com>
2022-02-07 21:11:38 -0500

    trying to connect

--- client/mock-front.js
+++ client/mock-front.js
@@ -10,40 +10,41 @@ let whiteCards = [];
 let blackCard = "";
 let czarAnswers = [];
 
-socket.on('connect', (socket) => {
-  console.log("you are connected via", socket.id);
+socket.on('connect', () => console.log('you are connected!'));
+// socket.on('connect', (socket) => {
+//   console.log("you are connected via", socket.id);
 
-  socket.on('client connect', (payload) => {
-    players.push(payload.socketid);
-  });
+//   socket.on('client connect', (payload) => {
+//     players.push(payload.socketid);
+//   });
 
-  socket.on('receive white cards', (payload) => {
-    whiteCards = payload.cards;
-  });
+//   socket.on('receive white cards', (payload) => {
+//     whiteCards = payload.cards;
+//   });
 
-  socket.on('game start', (payload) => {
-    blackCard = payload.card;
-    // Client choses choice from their white cards, sends to server, which sends array of 3 items to czar
-    let choice = whiteCards.shift();
-    socket.emit('choice', { choice: choice });
-    socket.on('choices', (payload) => {
-      czarAnswers = payload.czarAnswers;
-      socket.emit('round winner', { roundWinner: czarAnswers[0] });
-    });
-    // Up the count for round winner (server?)
-    socket.emit('draw white');
-    socket.on('draw white', (payload) => {
-      whiteCards.push(payload.card);
-    });
-  });
+//   socket.on('game start', (payload) => {
+//     blackCard = payload.card;
+//     // Client choses choice from their white cards, sends to server, which sends array of 3 items to czar
+//     let choice = whiteCards.shift();
+//     socket.emit('choice', { choice: choice });
+//     socket.on('choices', (payload) => {
+//       czarAnswers = payload.czarAnswers;
+//       socket.emit('round winner', { roundWinner: czarAnswers[0] });
+//     });
+//     // Up the count for round winner (server?)
+//     socket.emit('draw white');
+//     socket.on('draw white', (payload) => {
+//       whiteCards.push(payload.card);
+//     });
+//   });
 
-  socket.on('game end', () => {
-    socket.emit('forceDisconnect');
-  });
+//   socket.on('game end', () => {
+//     socket.emit('forceDisconnect');
+//   });
 
-  socket.on('disconnect', (payload) => {
-    players = players.sort((player) => {
-      player !== payload.player;
-    });
-  });
-});
+//   socket.on('disconnect', (payload) => {
+//     players = players.sort((player) => {
+//       player !== payload.player;
+//     });
+//   });
+// });
