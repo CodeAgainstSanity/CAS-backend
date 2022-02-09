23231ebf54dba32cd623178d740124d18cada502
Andrew Enyeart <reallyhardtoforget@gmail.com>
2022-02-08 22:10:19 -0800

    Suggests deletion of assignCzar.js

--- src/callbacks/assignCzar.js
+++ src/callbacks/assignCzar.js
@@ -1,8 +1,10 @@
-'use strict';
+// 'use strict';
 
-function assignCzar() {
-    let tempPlayer = { socketid: player[0] }.shift();
-    players.push(tempPlayer);
-    tempPlayer = players[0];
-    socket.to(tempPlayer).emit('Czar', 'You are the new Card Czar');
-};
+// function assignCzar() {
+//     let tempPlayer = { socketid: player[0] }.shift();
+//     players.push(tempPlayer);
+//     tempPlayer = players[0];
+//     socket.to(tempPlayer).emit('Czar', 'You are the new Card Czar');
+// };
+
+// TODO DELETE this unused module. Has bugs and not even being used
